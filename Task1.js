6f43a214743fddd65e969beda53df9928c317a92
chen62di <53136384+chen62di@users.noreply.github.com>
2019-07-21 15:10:25 +0300

    Update Task1.js

--- Task1.js
+++ Task1.js
@@ -2,7 +2,22 @@ const mySelf = {name: 'Chen Dimri',
                age: '21',
                adress: {city: 'Yavne',
                				country: "Israel"},
-      					family: [{name: 'Liza', age: '53'}, {name: 'Igal', age: '61'}, {name: 'Liran', age: '30'}, {name: 'Aviv', age: '27'}]};
+      				family: [{name: 'Liza', age: '53'}, {name: 'Igal', age: '61'}, {name: 'Liran', age: '30'}, {name: 'Aviv', age: '27'}],
+               getAdress() {
+                 return this.name
+               },
+               howManyRelatives() {
+                 return this.family.length
+               },
+               printMyName() {
+                 console.log(this.name)
+               },
+               addFamilyRelative(obj) {
+                 this.family.push()
+               },
+               setName(name) {
+                 this.name = name
+               }};
 
 
 function printObject(obj)
@@ -14,4 +29,4 @@ function printObject(obj)
 }
 
 printObject(mySelf)
-
+mySelf.printMyName()
