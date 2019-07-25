e9b45a79aff187653768307bef4b395fcee857a2
chen62di <53136384+chen62di@users.noreply.github.com>
2019-07-25 13:38:07 +0300

    Add files via upload

--- 24.7-Task3/sumCtrl.js
+++ 24.7-Task3/sumCtrl.js
@@ -3,14 +3,21 @@ module.controller("sumCtrl", SumCtrl);
 function SumCtrl($scope, dataService) {
     $scope.data = dataService;
     $scope.getClass = function() {
-        if($scope.data.result >= 0 && $scope.data.color === 'Green-Yellow') {
-            return 'green';
-        } else if ($scope.data.result < 0 && $scope.data.color === 'Green-Yellow') {
-            return 'yellow';
-        } else if ($scope.data.result >= 0 && $scope.data.color === 'Blue-Brown') {
-            return 'blue';
+        if($scope.data.a + $scope.data.b > 0) {
+            if ($scope.color === 'Green-Yellow') {
+                return 'green';
+            } else if ($scope.color === 'Blue-Brown') {
+                return 'blue';
+            }
+        } else if (($scope.data.a + $scope.data.b) < 0){
+            if ($scope.color === 'Green-Yellow') {
+                return 'yellow';
+            } else if ($scope.color === 'Blue-Brown') {
+                return 'brown';    
+            }
         } else {
-            return 'brown';
+            return '';
         }
+
     }
 }
