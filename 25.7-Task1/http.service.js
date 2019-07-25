d13e58ac9c08e9e7e0adce175e0c397644e0ac84
chen62di <53136384+chen62di@users.noreply.github.com>
2019-07-25 11:58:15 +0300

    Add files via upload

--- 25.7-Task1/http.service.js
+++ 25.7-Task1/http.service.js
@@ -3,7 +3,6 @@ module.service("httpService", function(dataService, urlService, $http) {
     this.getAllData = function() {
         let p = $http.get(urlService.url);
         p.then( (resp) => {
-            console.log(resp.data);
             dataService.todos = resp.data;
         }, (err) => {
             dataService.dataError = err;
@@ -13,7 +12,6 @@ module.service("httpService", function(dataService, urlService, $http) {
     this.getDataById = function(dataId) {
         let p = $http.get(`${urlService.url}/${dataId}`);
         p.then( (resp) => {
-            console.log(resp.data);
             dataService.someData = resp.data;
         }, (err) => {
             dataService.someDataError = err;
